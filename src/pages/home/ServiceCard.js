bee615ddc7d975b379a9b6010928fef5258c9cab
Md Hasan <mdhasan8064@gmail.com>
2022-11-08 23:35:31 +0600

    add photo viewer

--- src/pages/home/ServiceCard.js
+++ src/pages/home/ServiceCard.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import 'react-photo-view/dist/react-photo-view.css';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
 
 const ServiceCard = ({ service }) => {
     const { title, description, price, img } = service;
     return (
         <div className="card glass mb-5 hover:bg-slate-300 hover:shadow-lg duration-300 hover:translate-x-2 hover:translate-y-[-8px]">
-            <figure><img src={img} alt="car!" /></figure>
+            <PhotoProvider>
+                <PhotoView src={img}>
+                    <img src={img} alt="car!" />
+                </PhotoView>
+            </PhotoProvider>
             <div className="card-body">
                 <h2 className="card-title mb-2 text-red-600">Price: {price}</h2>
                 <h2 className="card-title">{title}</h2>
@@ -18,7 +24,7 @@ const ServiceCard = ({ service }) => {
                     <button className="border-none bg-blue-600 py-2 px-4 text-white font-medium rounded-lg hover:bg-red-400 duration-300 ">Dtails</button>
                 </div>
             </div>
-        </div>
+        </div >
     );
 };
 
