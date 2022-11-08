e93e22161ce39acd5bffad771720264a0c5a5ac7
Md Hasan <mdhasan8064@gmail.com>
2022-11-08 18:38:56 +0600

    add footer

---
+++ src/shared/Footer.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { FaFacebook, FaYoutube, FaGithub } from 'react-icons/fa';
+
+const Footer = () => {
+    return (
+        <footer className="footer footer-center p-6 ">
+            <div>
+                <p className="font-bold">
+                    Tourist Hasan
+                </p>
+                <p>Copyright © 2022 - All right reserved</p>
+            </div>
+            <div>
+                <div className="grid grid-flow-col gap-4">
+                    <a rel="noreferrer" target='_blank' href="https://github.com/mdhasan76" className='p-2 bg-slate-300 rounded-full'>
+                        <FaGithub className='text-2xl' />
+                    </a>
+                    <a rel="noreferrer" target='_blank' href="https://youtube.com" className='p-2 bg-slate-300 rounded-full'>
+                        <FaYoutube className='text-2xl text-red-600' />
+                    </a>
+                    <a rel="noreferrer" target='_blank' href="https://www.facebook.com/profile.php?id=100026677590809" className='p-2 bg-slate-300 rounded-full'> <FaFacebook className='text-2xl text-blue-600' /> </a>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
